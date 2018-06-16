af28ff8943aa3b6bf105018efc8fa04a750b7644
andrekandore <pyunpyun@mac.com>
2018-06-16 22:54:36 +0900

    Initial Commit

---
+++ IBDesignableTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBDesignableTest
+//
+//  Created by アンドレ on H30/06/16.
+//  Copyright © 平成30年 アンドレ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
